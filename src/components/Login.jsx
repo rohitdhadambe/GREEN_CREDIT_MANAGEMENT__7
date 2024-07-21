33467080dfb955f017a173973819d37e16945537
rohitdhadambe <rohit.dhadambe.ai@ghrcem.raisoni.net>
2024-07-21 11:58:50 +0530

    quetions update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-10   ">
-      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg transform transition-all duration-500 hover:scale-105  border-4 border-green-500">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg transform transition-all duration-500 hover:scale-105  border-2 border-green-500">
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-6 animate-fade-in">Login..!</h2>
         {error && <p className="text-center text-red-500 mb-4 animate-fade-in">{error}</p>}
         <form onSubmit={handleLogin} className="space-y-6">
